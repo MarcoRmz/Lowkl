633ad609f4c1f8656c523bd0bf1cb83297848824
Jorge Luis Perales <jorgeluis070294@hotmail.com>
2016-08-27 17:08:51 -0500

    Map view addition

---
+++ Lowkl/Lowkl/Lowlk-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Lowlk-Bridging-Header.h
+//  Lowkl
+//
+//  Created by Jorge Luis Perales on 27/08/16.
+//  Copyright © 2016 Jorge Luis Perales. All rights reserved.
+//
+
+#ifndef Lowlk_Bridging_Header_h
+#define Lowlk_Bridging_Header_h
+
+#import "GeoFire.h"
+
+#endif /* Lowlk_Bridging_Header_h */

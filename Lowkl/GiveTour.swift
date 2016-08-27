11d570167ad20c6c47b987930ea0bb819aba51f6
Alejandro Sanchez <alejandro@axented.com>
2016-08-27 17:01:22 -0500

    Start give tour

---
+++ Lowkl/GiveTour.swift
@@ -0,0 +1,24 @@
+//
+//  GiveTour.swift
+//  Lowkl
+//
+//  Created by Alejandro Sanchez on 8/27/16.
+//  Copyright © 2016 Jorge Luis Perales. All rights reserved.
+//
+
+import UIKit
+
+class GiveTour: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+

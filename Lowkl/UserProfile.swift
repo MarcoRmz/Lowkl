dbc3464a57ee5dab1f9cb77bbc608a2feb52fd26
Alejandro Sanchez <alejandro@axented.com>
2016-08-27 18:57:08 -0500

    Add userProfile

---
+++ Lowkl/UserProfile.swift
@@ -0,0 +1,70 @@
+//
+//  UserProfile.swift
+//  Lowkl
+//
+//  Created by Alejandro Sanchez on 8/27/16.
+//  Copyright © 2016 Jorge Luis Perales. All rights reserved.
+//
+
+import UIKit
+
+class UserProfile: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var imageUser: UIImageView!
+    @IBOutlet var nameUser: UILabel!
+    
+    @IBOutlet var tableViewTaken: UITableView!
+    @IBOutlet var tableViewGiven: UITableView!
+    
+    var  taken = [String]()
+    var  given = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableViewTaken.delegate = self
+        self.tableViewTaken.dataSource = self
+        
+        taken = ["Bahamas", "Kualalupur", "Fiji"]
+        given = ["Monterrey","Cancun"]
+        
+        self.tableViewGiven.delegate = self
+        self.tableViewGiven.dataSource = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        if(tableView.isEqual(tableViewTaken)){
+            let cell = tableView.dequeueReusableCellWithIdentifier("CellToursTaken", forIndexPath: indexPath)
+            let take = taken[indexPath.row]
+            cell.textLabel?.text = take
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCellWithIdentifier("CellToursGiven", forIndexPath: indexPath)
+            let give = given[indexPath.row]
+            cell.textLabel?.text = give
+            return cell
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(tableView.isEqual(tableViewTaken)){
+            return taken.count
+        }else{
+            return given.count
+        }
+        
+    }
+    
+    
+    
+}

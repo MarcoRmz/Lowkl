f538a95de1d19eb24e8541187035afcc91de462d
Marco <MarcoRmz@users.noreply.github.com>
2016-08-28 10:21:54 -0500

    created guide table and populated guide tours table

--- Lowkl/UserProfile.swift
+++ Lowkl/UserProfile.swift
@@ -49,6 +49,7 @@ class UserProfile: UIViewController, UITableViewDelegate, UITableViewDataSource 
         //Check if user is guide or not
         if (!self.guide!) {
             self.userDatabaseRef.child(self.userID + "/guide").setValue(true)
+            FIRDatabase.database().reference().child("guides").child(self.userID)
             guide = true
         }
         

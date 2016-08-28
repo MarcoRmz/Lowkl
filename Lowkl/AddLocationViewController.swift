f4cf9350ea587d4863179ab333a0035141594eb7
Jorge Luis Perales <jorgeluis070294@hotmail.com>
2016-08-28 09:33:49 -0500

    Fixed pin dropping

--- Lowkl/AddLocationViewController.swift
+++ Lowkl/AddLocationViewController.swift
@@ -26,9 +26,10 @@ class AddLocationViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mapView.delegate = self
+        toursFireRef = FIRDatabase.database().reference().child("tours")
         geoFireRef = FIRDatabase.database().reference().child("locations")
         geofire = GeoFire(firebaseRef: geoFireRef)
-         toursFireRef = FIRDatabase.database().reference().child("tours")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,8 +43,27 @@ class AddLocationViewController: UIViewController, MKMapViewDelegate {
     
     @IBAction func addLocationButtonPressed(sender: UIButton) {
         let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
-        let rand = arc4random_uniform(4) + 1
-        createSighting(forLocation: loc, withId: Int(rand), titleName: self.nameTextField.text!, description: self.descriptionTextField.text!)
+        var count: Int = 0
+        self.toursFireRef.observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+            // Get user value
+            print(snapshot)
+            print("ok")
+            
+            let tours = snapshot.children
+            print(tours)
+            
+            for child in snapshot.children {
+                count = count + 1
+            }
+            print(count)
+            // ...
+            self.createSighting(forLocation: loc, withId: count, titleName: self.nameTextField.text!, description: self.descriptionTextField.text!)
+        }) { (error) in
+            print("-----")
+            print(error.localizedDescription)
+        }
+
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
@@ -54,11 +74,12 @@ class AddLocationViewController: UIViewController, MKMapViewDelegate {
     
     func createSighting(forLocation location: CLLocation, withId tourId: Int, titleName: String, description: String) {
         //geofire.setLocation(location, forKey: "\(tourId)")
-        
+        print("create sighting")
         geofire.setLocation(location, forKey: "\(tourId)", withCompletionBlock:  { (error) in
             if error != nil {
                 print("error")
             } else {
+                print("Se guarda esto")
                 self.toursFireRef.child("\(tourId)").child("name").setValue("\(titleName)")
                 self.toursFireRef.child("\(tourId)").child("description").setValue("\(description)")
             }
--- Lowkl/TourAnnotation.swift
+++ Lowkl/TourAnnotation.swift
@@ -8,8 +8,12 @@
 
 import Foundation
 import MapKit
+import FirebaseDatabase
 
-let randomNames = ["Paseo Tec", "Cintermex", "Tec", "Fundidora"]
+let randomNames = ["Paseo Tec", "Cintermex", "Tec", "Fundidora", "Test", "Test dos"]
+
+let dbRef = FIRDatabase.database().reference()
+
 
 class TourAnnotation: NSObject, MKAnnotation {
     var coordinate = CLLocationCoordinate2D()
@@ -17,10 +21,12 @@ class TourAnnotation: NSObject, MKAnnotation {
     var tourName: String
     var title: String?
     
-    init(coordinate: CLLocationCoordinate2D, tourNumber: Int) {
+    
+    init(coordinate: CLLocationCoordinate2D, tourNumber: Int, locationArray: [String]) {
         self.coordinate = coordinate
         self.tourNumber = tourNumber
-        self.tourName = randomNames[tourNumber - 1]
+        //self.tourName = locationArray
+        self.tourName = locationArray[tourNumber]
         self.title = self.tourName
     }
 }
